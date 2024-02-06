39996503271807100a0a70d19edc2426f61937f5
Cengizhan Akgün <coderista@gmail.com>
2024-02-06 23:34:09 +0300

    Add project files.

---
+++ MVCEvrakTakipUygulamasi/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCEvrakTakipUygulamasi.Models;
+
+namespace MVCEvrakTakipUygulamasi.Controllers
+{
+    public class LoginController : Controller
+    {
+        MVCEvrakTakipDBEntities1 entities1 = new MVCEvrakTakipDBEntities1();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
